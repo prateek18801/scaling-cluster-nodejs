c4edd194cb6f5cdd91a74a06d5003a368bbc51cb
Prateek Chaurasia <prateek18801@gmail.com>
2022-08-13 07:43:33 +0530

    spawn multiple worker instances

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const cluster = require('node:cluster');
+const os = require('node:os');
 const express = require('express');
 
 const app = express();
@@ -5,10 +7,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.status(200).send(`process: ${process.pid}`);
 });
 
-app.listen(3000, () => {
-    console.log(`process ${process.pid} running on 0.0.0.0:3000`);
-});
+if (cluster.isPrimary) {
+    console.log(`master ${process.pid} initialising cluster`);
+
+    for (let i = 0; i < os.cpus().length; i++) {
+        cluster.fork();
+    }
+
+} else {
+    app.listen(3000, () => {
+        console.log(`worker ${process.pid} started on port 3000`);
+    });
+}
